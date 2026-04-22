9ab12939af42cb029e38d2b0f7ed198175982cd6
vivu <46775082+ufelmann@users.noreply.github.com>
2026-04-22 08:08:50 +0200

    fix(ui): restore graph route shell

--- knowledge-ui/src/pages/GraphRoute.vue
+++ knowledge-ui/src/pages/GraphRoute.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import IconRail from '../components/shell/IconRail.vue'
+import SlidePanel from '../components/shell/SlidePanel.vue'
+import SearchPanel from '../components/shell/SearchPanel.vue'
+import RealmsPanel from '../components/shell/RealmsPanel.vue'
+import SettingsPanel from '../components/shell/SettingsPanel.vue'
 import ScanPanel from '../components/ScanPanel.vue'
 import Reader from '../components/Reader.vue'
 import { useCanvasStore } from '../stores/canvas'
@@ -18,6 +22,9 @@ useKeybindings()
 <template>
   <div class="graph-root">
     <IconRail />
+    <SlidePanel id="search" title="Search"><SearchPanel /></SlidePanel>
+    <SlidePanel id="realms" title="Realms"><RealmsPanel /></SlidePanel>
+    <SlidePanel id="settings" title="Settings"><SettingsPanel /></SlidePanel>
     <main class="graph-slot">Graph view placeholder</main>
     <ScanPanel />
     <Reader />
